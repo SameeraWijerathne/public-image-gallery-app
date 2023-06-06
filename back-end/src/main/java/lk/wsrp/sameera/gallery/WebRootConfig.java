a767702b0b9c9f42ea5cac7e3d7454122c9b27b4
Sameera <sameerawncd@gmail.com>
2023-06-07 00:44:47 +0530

    Initial commit - project stucture completed

---
+++ back-end/src/main/java/lk/wsrp/sameera/gallery/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.wsrp.sameera.gallery;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+public class WebRootConfig {
+}
