884ec2833582c699f51c2eb0f85851e76b1d58bf
Sameera <sameerawncd@gmail.com>
2023-06-07 09:25:52 +0530

    image drag and drop feature implemented

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -16,6 +16,19 @@ $(document).on('keyup', (eventData)=>{
     if (eventData.key === 'Escape' && !overlay.hasClass('d-none')) overlay.addClass('d-none');
 });
 
+dropZoneElm.on('dragover', (evt)=> {
+    evt.preventDefault();
+});
+dropZoneElm.on('drop', (evt)=> {
+    evt.preventDefault();
+    const droppedFiles = evt.originalEvent.dataTransfer.files;
+    const imageFiles = Array.from(droppedFiles).filter(file => file.type.startsWith("image/"));
+    if (!imageFiles.length) return;
+    overlay.addClass('d-none');
+});
+overlay.on('dragover', (evt) => evt.preventDefault());
+overlay.on('drop', (evt) => evt.preventDefault());
+
 function loadAllImages(){
     const jqxhr = $.ajax(`${REST_API_URL}/images`);
     jqxhr.done((imageUrlList)=>{
